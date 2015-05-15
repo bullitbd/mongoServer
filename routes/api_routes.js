01b5a042c4fa3438d7e14b249849d47ccb70102f
bullitbd <bobd@rialtoadvisors.com>
2015-05-15 10:20:24 -0700

    commit tests

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,17 +1,17 @@
 'use strict'; // Notes
 
-var express = require('express');
-var api = express.Router();
+
 var Entry = require('../models/entry_model');
 
 var bodyparser = require('body-parser');
 
-module.exports = function() {
 
-api.use(bodyparser.json());
+module.exports = function(router) {
+
+    router.use(bodyparser.json());
 
     // get
-    api.get('/entries', function(req, res) {
+    router.get('/entries', function(req, res) {
         Entry.find({}, function(err, data)  {
             if (err) {
                 res.status(500).send({msg: 'couldn\'t find data'});
@@ -22,21 +22,22 @@ api.use(bodyparser.json());
         // find all
     });
 
-    api.get('/entries/:id', function(req, res) {
+    router.get('/entries/:id', function(req, res) {
+
         Entry.find({_id: req.params.id}, function(err, data)  {
             if (err) {
                 res.status(500).send({msg: 'couldn\'t find data'});
                 return;
             }
             res.json(data);
-        })
+        });
     });
 
 
 
     // post
     // simple body post - writeFile...
-    api.post('/entries', function(req, res) {
+    router.post('/entries', function(req, res) {
         var newEntry = new Entry(req.body);
         newEntry.save(function(err, data) {
             if(err) {res.status(500).send({msg: 'couldn\'t save post'});
@@ -49,28 +50,31 @@ api.use(bodyparser.json());
     // put
     // ideally, we need to get a resource to be edited;
     // GET then PUT
-    api.put('/entries/:id', function(req, res) {
+    router.put('/entries/:id', function(req, res) {
         var updated = req.body;
         delete updated._id;
-        Entry.update({_id:req.parms.id}, updated, function(err) {
+        Entry.update({_id: req.params.id}, updated, function(err, data) {
             if (err) {
                 res.status(500).send({msg: 'couldn\'t save post'});
                 return;
             }
-            res.json({msg: 'updated!'});
-        })
+            res.json(data);
+        });
     });
 
 
 
     // delete
     // in this case, delete file;
-    api.delete('/entries/:id', function(req, res) {
-        Entry.remove({_id:req.parms.id}, function(err) {
+    router.delete('/entries/:id', function(req, res) {
+        Entry.remove({_id:req.params.id}, function(err, data) {
             if (err) {
                 res.status(500).send({msg: 'couldn\'t delete entry'});
                 return;
             }
+            res.json(data);
         });
     });
+
+
 };
